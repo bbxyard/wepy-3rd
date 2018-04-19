67b22c2903eb9148accce339caf4246e086a214e
bbxyard <bbxyard@gmail.com>
2018-04-19 15:09:19 +0800

    app-emit-update-3rd ${REASON} $(date +%Y-%m-%d-%H-%m-%S)

--- examples/prv/config/xb.js
+++ examples/prv/config/xb.js
@@ -7,11 +7,11 @@ const mine = {
     hd: { title: '我的订单', desc: '查看全部订单' },
     bd: {
       list: [
-        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon1.png', fa: '', txt: '待付款', cnt: 0 },
-        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon2.png', fa: '', txt: '待发货', cnt: 1 },
-        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon3.png', fa: '', txt: '已发货', cnt: 5 },
-        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon4.png', fa: '', txt: '待评价', cnt: 8 },
-        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon5.png', fa: '', txt: '退款/售后', cnt: 0 }
+        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon1.png', fa: '', title: '待付款', cnt: 0 },
+        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon2.png', fa: '', title: '待发货', cnt: 1 },
+        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon3.png', fa: '', title: '已发货', cnt: 5 },
+        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon4.png', fa: '', title: '待评价', cnt: 8 },
+        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon5.png', fa: '', title: '退款/售后', cnt: 0 }
       ]
     }
   },
@@ -19,14 +19,14 @@ const mine = {
     hd: { title: '我的服务', desc: '' },
     bd: {
       width: '25%',      list: [
-        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon6.png', fa: '', txt: '优惠券', cnt: 0 },
-        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon7.png', fa: '', txt: '收藏夹', cnt: 3 },
-        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon8.png', fa: '', txt: '商品点评', cnt: 0 },
-        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon9.png', fa: '', txt: '联系客服', cnt: 0 },
-        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon10.png', fa: '', txt: '地址管理', cnt: 0 },
-        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon11.png', fa: '', txt: '修改密码', cnt: 0 },
-        // { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon12.png', fa: '', txt: '帮助', cnt: 0 },
-        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon13.png', fa: '', txt: '设置', cnt: 0 }
+        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon6.png', fa: '', title: '优惠券', cnt: 0 },
+        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon7.png', fa: '', title: '收藏夹', cnt: 3 },
+        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon8.png', fa: '', title: '商品点评', cnt: 0 },
+        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon9.png', fa: '', title: '联系客服', cnt: 0 },
+        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon10.png', fa: '', title: '地址管理', cnt: 0 },
+        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon11.png', fa: '', title: '修改密码', cnt: 0 },
+        // { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon12.png', fa: '', title: '帮助', cnt: 0 },
+        { url: '', img: 'http://img02.camel.com.cn/image/zwuc_icon13.png', fa: '', title: '设置', cnt: 0 }
       ]
     }
   }
