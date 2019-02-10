64556fe5ecf840cd1ad222a605ff8738f517bd07
bbxyard <bbxyard@gmail.com>
2019-02-10 17:22:02 +0800

    add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  // required to lint *.wpy files
+  plugins: [
+    'html'
+  ],
+  settings: {
+    'html/html-extensions': ['.html', '.wpy']
+  },
+  'globals': {
+    'wx': true
+  },
+  // add your custom rules here
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    "semi": ["warn", "always"],
+    // "semi": ["error", "never"],
+    "no-multi-spaces": 0,
+    "padded-blocks": 0,
+    // 'indent': ['error', 2],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'space-before-function-paren': 0
+  }
+}
